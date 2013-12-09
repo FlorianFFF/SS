8d1f2b0cb8ca009ddfdaeb9e4729859f2b43c64a
FlorianFFF <florianffff@gmail.com>
2013-12-09 16:13:23 +0100

    test

---
+++ src/ss/week4/Polynomial.java
@@ -0,0 +1,21 @@
+package ss.week4;
+
+public class Polynomial implements Function {
+	int parts[];
+	public Polynomial(int parts[]) {
+		this.parts = parts;
+	}
+	
+	@Override
+	public int apply(int value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Function derivative(Function function) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
